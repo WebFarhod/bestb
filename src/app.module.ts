32d3516667434f969c1563534aaf26d97e89bc33
WebFarhod <https://github.com/WebFarhod>
2024-06-29 22:00:45 +0500

    news oo

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ContactModule } from './contact/contact.module';
 import { OptionsModule } from './options/options.module';
 import { ProgramsModule } from './programs/programs.module';
 import { ClassesModule } from './classes/classes.module';
-import { NewsModule } from './news/news.module';
+// import { NewsModule } from './news/news.module';
 // import { ClassesModule } from './classes/classes.module';
 
 @Module({
@@ -20,7 +20,7 @@ import { NewsModule } from './news/news.module';
     TeachersModule,
     ClassesModule,
     ProgramsModule,
-    NewsModule,
+    // NewsModule,
     // FilesModule,
     UsersModule,
     ContactModule,
