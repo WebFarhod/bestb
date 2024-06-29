b1fc5c81383305ddd0c59890b6233bdacdb9ce98
WebFarhod <https://github.com/WebFarhod>
2024-06-29 23:37:44 +0500

    remove teacher 6

--- src/classes/classes.controller.ts
+++ src/classes/classes.controller.ts
@@ -1,32 +1,32 @@
 import {
   Controller,
   // Get,
-  Post,
-  Body,
+  // Post,
+  // Body,
   // Param,
   // Delete,
   // Put,
-  UseGuards,
+  // UseGuards,
 } from '@nestjs/common';
 import { ClassesService } from './classes.service';
-import { CreateClassDto } from './dto/create-class.dto';
+// import { CreateClassDto } from './dto/create-class.dto';
 
-// import { ClassResponseType } from './types/classResponse.type';
-import { Roles } from 'src/auth/decorator/roles.decorator';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { RolesGuard } from 'src/auth/guards/roles.guard';
-import { Role } from 'src/enums/role.enum';
+// // import { ClassResponseType } from './types/classResponse.type';
+// import { Roles } from 'src/auth/decorator/roles.decorator';
+// import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+// import { RolesGuard } from 'src/auth/guards/roles.guard';
+// import { Role } from 'src/enums/role.enum';
 
 @Controller('classes')
 export class ClassesController {
   constructor(private readonly classesService: ClassesService) {}
 
-  @Roles(Role.ADMIN)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Post()
-  createClass(@Body() data: CreateClassDto) {
-    return this.classesService.createClass(data);
-  }
+  // @Roles(Role.ADMIN)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Post()
+  // createClass(@Body() data: CreateClassDto) {
+  //   return this.classesService.createClass(data);
+  // }
 
   // @Roles(Role.ADMIN)
   // @UseGuards(JwtAuthGuard, RolesGuard)
