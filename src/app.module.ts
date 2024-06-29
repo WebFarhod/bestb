1cd44eee535c9ae053808845b3aa0e3e5c0df871
WebFarhod <https://github.com/WebFarhod>
2024-06-29 15:30:02 +0500

    add teacher

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TeachersModule } from './teachers/teachers.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
-    // TeachersModule,
+    TeachersModule,
     // ClassesModule,
     // ProgramsModule,
     // NewsModule,
