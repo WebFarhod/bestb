751e5ac99febfc2d7a446b1e916e8013ed79d63e
WebFarhod <https://github.com/WebFarhod>
2024-06-29 22:09:21 +0500

    new test 0

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,7 @@ import { ContactModule } from './contact/contact.module';
 import { OptionsModule } from './options/options.module';
 import { ProgramsModule } from './programs/programs.module';
 import { ClassesModule } from './classes/classes.module';
-// import { NewsModule } from './news/news.module';
-// import { ClassesModule } from './classes/classes.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
   imports: [
@@ -20,7 +19,7 @@ import { ClassesModule } from './classes/classes.module';
     TeachersModule,
     ClassesModule,
     ProgramsModule,
-    // NewsModule,
+    NewsModule,
     // FilesModule,
     UsersModule,
     ContactModule,
