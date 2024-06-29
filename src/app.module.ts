32091a966c989d5640b570006ce5ab2a7a42bfea
WebFarhod <https://github.com/WebFarhod>
2024-06-29 15:21:37 +0500

    add user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+    // TeachersModule,
+    // ClassesModule,
+    // ProgramsModule,
+    // NewsModule,
+    // FilesModule,
+    UsersModule,
+    // ContactModule,
+    // CommentsModule,
+    // MessageModule,
+    // OptionsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
