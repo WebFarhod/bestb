12ad9804544a431c93fab2c9683e72de58c27fad
WebFarhod <https://github.com/WebFarhod>
2024-06-30 10:01:57 +0500

    class fix test 6

--- src/classes/classes.controller.ts
+++ src/classes/classes.controller.ts
@@ -16,15 +16,15 @@ import { Roles } from 'src/auth/decorator/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Role } from 'src/enums/role.enum';
-import { TeachersService } from 'src/teachers/teachers.service';
-import { ProgramsService } from 'src/programs/programs.service';
+// import { TeachersService } from 'src/teachers/teachers.service';
+// import { ProgramsService } from 'src/programs/programs.service';
 
 @Controller('classes')
 export class ClassesController {
   constructor(
     private readonly classesService: ClassesService,
-    private readonly teacherService: TeachersService,
-    private readonly programService: ProgramsService,
+    // private readonly teacherService: TeachersService,
+    // private readonly programService: ProgramsService,
   ) {}
 
   @Roles(Role.ADMIN)
@@ -44,10 +44,10 @@ export class ClassesController {
   // @Get()
   // async findAll() {
   //   const data = await this.classesService.findAll();
-
+  //   // return data;
   //   const response = data.map(async (doc) => {
-  //     const program = await this.programService.findOne(doc.type);
-  //     const teacher = await this.teacherService.findOne(doc.teacher);
+  //     // const program = await this.programService.findOne(doc.type);
+  //     // const teacher = await this.teacherService.findOne(doc.teacher);
 
   //     return {
   //       _id: doc._id.toString(),
@@ -56,18 +56,23 @@ export class ClassesController {
   //       description: doc.description,
   //       about: doc.about,
   //       type: doc.type,
-  //       price: program ? program.price : null,
-  //       infos: program ? program.infos : null,
-  //       teacher: teacher
-  //         ? {
-  //             _id: teacher._id.toString(),
-  //             name: teacher.name,
-  //             surname: teacher.surname,
-  //             image: teacher.image,
-  //           }
-  //         : null,
+  //       // price: program ? program.price : null,
+  //       // infos: program ? program.infos : null,
+  //       // price: program ? program.price : null,
+  //       // infos: program ? program.infos : null,
+  //       // teacher: teacher,
+  //       // ? {
+  //       //     _id: teacher._id.toString(),
+  //       //     name: teacher.name,
+  //       //     surname: teacher.surname,
+  //       //     image: teacher.image,
+  //       //   }
+  //       // : null,
   //     };
   //   });
+  //   console.log('====================================');
+  //   console.log(response);
+  //   console.log('====================================');
 
   //   return response;
   // }
