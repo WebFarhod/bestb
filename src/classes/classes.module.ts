c21f01f7633aaa4cca0ed54ec8971aca54822a3a
WebFarhod <https://github.com/WebFarhod>
2024-06-30 10:47:53 +0500

    class fix test 11

--- src/classes/classes.module.ts
+++ src/classes/classes.module.ts
@@ -1,16 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { ClassesController } from './classes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Class, ClassSchema } from './schema/class.schema';
 import { ProgramSchema, Programs } from 'src/programs/schema/program.schema';
 import { Teacher, TeacherSchema } from 'src/teachers/schema/teacher.schema';
-// import { ProgramsService } from 'src/programs/programs.service';
-// import { TeachersService } from 'src/teachers/teachers.service';
-// import { TeachersService } from 'src/teachers/teachers.service';
-// import { ProgramsService } from 'src/programs/programs.service';
-// import { TeachersModule } from 'src/teachers/teachers.module';
-// import { ProgramsModule } from 'src/programs/programs.module';
+import { TeachersModule } from 'src/teachers/teachers.module';
+import { ProgramsModule } from 'src/programs/programs.module';
 
 @Module({
   imports: [
@@ -30,8 +26,8 @@ import { Teacher, TeacherSchema } from 'src/teachers/schema/teacher.schema';
     ]),
     // TeachersModule,
     // ProgramsModule,
-    // forwardRef(() => TeachersModule),
-    // forwardRef(() => ProgramsModule),
+    forwardRef(() => TeachersModule),
+    forwardRef(() => ProgramsModule),
   ],
   controllers: [ClassesController],
   providers: [ClassesService],
